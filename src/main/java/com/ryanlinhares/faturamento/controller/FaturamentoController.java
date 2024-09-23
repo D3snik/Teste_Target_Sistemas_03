70041bab7d4a598b185afb8cafff08070b1d0975
D3snik <ryandesnik45@gmail.com>
2024-09-23 20:51:53 -0300

    Conserto controllere outros pequenos ajuste finais

--- src/main/java/com/ryanlinhares/faturamento/controller/FaturamentoController.java
+++ src/main/java/com/ryanlinhares/faturamento/controller/FaturamentoController.java
@@ -1,45 +1,25 @@
 package com.ryanlinhares.faturamento.controller;
 
-import java.io.File;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ryanlinhares.faturamento.model.Faturamentos;
+import com.ryanlinhares.faturamento.services.FaturamentoService;
 
 @RestController
+@RequestMapping("/faturamento")
 public class FaturamentoController {
 
-    @GetMapping("/faturamento")
-    public String calcularFaturamento() throws JAXBException {
-        File file = new File("src/main/resources/faturamento.xml");
+    @Autowired
+    private FaturamentoService faturamentoService;
 
-        JAXBContext jaxbContext = JAXBContext.newInstance(Faturamentos.class);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        Faturamentos faturamentos = (Faturamentos) unmarshaller.unmarshal(file);
-
-        List<Faturamentos.Faturamento> listaFaturamento = faturamentos.getFaturamentoList();
-
-        List<Double> valoresValidos = listaFaturamento.stream()
-                .map(Faturamentos.Faturamento::getValor)
-                .filter(valor -> valor > 0)
-                .collect(Collectors.toList());
-
-        double menor = valoresValidos.stream().min(Double::compare).orElse(0.0);
-        double maior = valoresValidos.stream().max(Double::compare).orElse(0.0);
-        double media = valoresValidos.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
-
-        long diasAcimaDaMedia = valoresValidos.stream().filter(valor -> valor > media).count();
-
-        return String.format(
-            "Menor Faturamento: R$ %.2f\nMaior Faturamento: R$ %.2f\nDias Acima da Média: %d",
-            menor, maior, diasAcimaDaMedia
-        );
+    @GetMapping("/processar")
+    public ResponseEntity<String> processarFaturamento() {
+        System.out.println("Requisição para processar faturamento recebida.");
+        faturamentoService.processarFaturamento();
+        return ResponseEntity.ok("Faturamento processado com sucesso.");
     }
+
 }

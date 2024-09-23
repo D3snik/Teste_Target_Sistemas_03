d9df1ec3a4e8771be8d287d94a12948082248726
D3snik <ryandesnik45@gmail.com>
2024-09-23 19:20:44 -0300

    First commit

---
+++ src/main/java/com/ryanlinhares/faturamento/TesteTargetSistemas03Application.java
@@ -0,0 +1,13 @@
+package com.ryanlinhares.faturamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTargetSistemas03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTargetSistemas03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanlinhares/faturamento/TesteTargetSistemas03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanlinhares.faturamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTargetSistemas03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
